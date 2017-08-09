aa38166ce9291055b3e5311805afec969ce445a5
SobiaIdrees <s.idrees@student.unsw.edu.au>
2017-08-09 17:01:45 +1000

    Added GNU license info

--- ui.R
+++ ui.R
@@ -18,12 +18,12 @@
 #V1.0.5 - Added a new tab to show distribution of ELMs in the predicted DMI dataset in tabular as well as in interactive view.
 #V1.0.7 - File headers to lowercase for consistency
 ##############################
-#This program is free software: you can redistribute it and/or modify
+#SLiMEnrich is free software: you can redistribute it and/or modify
  #   it under the terms of the GNU General Public License as published by
   #  the Free Software Foundation, either version 3 of the License, or
   #  (at your option) any later version.
 
-   # This program is distributed in the hope that it will be useful,
+   # SLiMEnrich program is distributed in the hope that it will be useful,
    # but WITHOUT ANY WARRANTY; without even the implied warranty of
    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    # GNU General Public License for more details.
