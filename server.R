9d19084bff27ba233ae47ff193fe5e7c517733fc
SobiaIdrees <s.idrees@student.unsw.edu.au>
2017-08-09 16:19:13 +1000

    Added GNU license info

--- server.R
+++ server.R
@@ -19,7 +19,18 @@
 #V1.0.6 - Added p-values, GOTerms and Biological function of ELMs in ELM distribution tab
 #V1.0.7 - File headers to lowercase for consistency
 ##############################
+#This program is free software: you can redistribute it and/or modify
+ #   it under the terms of the GNU General Public License as published by
+  #  the Free Software Foundation, either version 3 of the License, or
+  #  (at your option) any later version.
 
+   # This program is distributed in the hope that it will be useful,
+   # but WITHOUT ANY WARRANTY; without even the implied warranty of
+   # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   # GNU General Public License for more details.
+
+   # You should have received a copy of the GNU General Public License
+   # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##############################
 #Required Libraries
 ##############################
